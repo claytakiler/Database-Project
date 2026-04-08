cb074f71570a4a71907b693d6c5ef1ca6d261734
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-08 14:41:06 -0500

    added columns to inserts

--- hearing/hearing_tuples.sql
+++ hearing/hearing_tuples.sql
@@ -1,24 +1,62 @@
 -- source hearing_tuples.sql; to run script
 
-insert into hearing values (‘00001’, ’Fontenot’, ’2026-1-3 11:00:00.00’, ’Baton Rouge City Court’);
-insert into hearing values (‘00001’, ’Fontenot’, ’2026-5-30 09:00:00.00’, ’Baton Rouge City Court’);
+-- case_ID = 00001
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00001’, ’Fontenot’, ’2026-1-3 11:00:00.00’, ’Baton Rouge City Court’);
+insert into hearing
+  (case_ID, judge, date_time, courthouse)
+  values (‘00001’, ’Fontenot’, ’2026-5-30 09:00:00.00’, ’Baton Rouge City Court’);
 
-insert into hearing values (‘00002’, ’Johnson’, ’2026-1-11 14:30:00.00’, ’New Orleans City Court’);
-insert into hearing values (‘00002’, ’Johnson’, ’2026-4-20 09:30:00.00’, ’New Orleans City Court’);
+-- case_ID = 00002
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00002’, ’Johnson’, ’2026-1-11 14:30:00.00’, ’New Orleans City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00002’, ’Johnson’, ’2026-4-20 09:30:00.00’, ’New Orleans City Court’);
 
-insert into hearing values (‘00003’, ’Smith’, ’2026-6-12 12:30:00.00’, ’Baton Rouge City Court’);
+-- case_ID = 00003
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00003’, ’Smith’, ’2026-6-12 12:30:00.00’, ’Baton Rouge City Court’);
 
-insert into hearing values (‘00005’, ’Broussard’, ’2026-5-2 11:00:00.00’, ’Lafayette City Court’);
-insert into hearing values (‘00005’, ’Broussard’, ’2026-6-27 09:30:00.00’, ’Lafayette City Court’);
+-- case_ID = 00005
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00005’, ’Broussard’, ’2026-5-2 11:00:00.00’, ’Lafayette City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00005’, ’Broussard’, ’2026-6-27 09:30:00.00’, ’Lafayette City Court’);
 
-insert into hearing values (‘00006’, ’Williams’, ’2026-6-25 11:00:00.00’, ’New Iberia City Court’);
+-- case_ID = 00006
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00006’, ’Williams’, ’2026-6-25 11:00:00.00’, ’New Iberia City Court’);
 
-insert into hearing values (‘00008’, ’Boudreaux’, ’2026-1-29 10:30:00.00’, ’Lafayette City Court’);
-insert into hearing values (‘00008’, ’Hebert’, ’2026-4-6 12:30:00.00’, ’Lafayette City Court’);
-insert into hearing values (‘00008’, ’Hebert’, ’2026-5-2 15:30:00.00’, ’Lafayette City Court’);
+-- case_ID = 00008
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00008’, ’Boudreaux’, ’2026-1-29 10:30:00.00’, ’Lafayette City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00008’, ’Hebert’, ’2026-4-6 12:30:00.00’, ’Lafayette City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00008’, ’Hebert’, ’2026-5-2 15:30:00.00’, ’Lafayette City Court’);
 
-insert into hearing values (‘00014’, ’Bourgeois’, ’2026-2-08 13:00:00.00’, ’New Iberia City Court’);
+-- case_ID = 00014
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00014’, ’Bourgeois’, ’2026-2-08 13:00:00.00’, ’New Iberia City Court’);
 
-insert into hearing values (‘00015’, ’LeBlanc’, 2026-3-3 09:00:00.00, ’New Orleans City Court’);
-insert into hearing values (‘00015’, ’LeBlanc’, ’2026-3-27 15:00:00.00’, ’New Orleans City Court’);
-insert into hearing values (‘00015’, ’Guidry’, ’2026-5-5 11:30:00.00’, ’New Orleans City Court’);
+-- case_ID = 00015
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00015’, ’LeBlanc’, 2026-3-3 09:00:00.00, ’New Orleans City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00015’, ’LeBlanc’, ’2026-3-27 15:00:00.00’, ’New Orleans City Court’);
+insert into hearing 
+  (case_ID, judge, date_time, courthouse)
+  values (‘00015’, ’Guidry’, ’2026-5-5 11:30:00.00’, ’New Orleans City Court’);
