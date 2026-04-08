4691baf0440939150a7ce57abcf78815d0c40db4
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-08 16:42:38 -0500

    added hearing table to schema

--- schema/client.sql
+++ schema/client.sql
@@ -7,3 +7,14 @@ CREATE TABLE client(
 	PRIMARY KEY (client_ID),
 	CHECK (type IN('individual', 'corporate'))
 );
+
+-- requires case table
+create table hearing (
+case_ID		char(5),
+judge			varchar(20),
+date_time		timestamp,
+courthouse		varchar(20),
+
+primary key(case_ID, date_time),
+foreign key (case_ID) references case(case_ID)
+);
