9c0aa4e1d1a8e96b4ec10ff8e7f72f40d524abff
MNguyen2003 <148157177+MNguyen2003@users.noreply.github.com>
2026-04-08 12:01:12 -0500

    Create specialization_tuples.sql

---
+++ lawyer_specialization/specialization_tuples.sql
@@ -0,0 +1,8 @@
+Insert into lawyer_specialization values (‘00001’, ‘corporate law’);
+Insert into lawyer_specialization values (‘00001’, ‘property law’);
+Insert into lawyer_specialization values (‘00002’, ‘criminal law’);
+Insert into lawyer_specialization values (‘00002’, ‘family law’);
+Insert into lawyer_specialization values (‘00003’, ‘corporate law’);
+Insert into lawyer_specialization values (‘00003’, ‘criminal law’);
+Insert into lawyer_specialization values (‘00003’, ‘property  law’);
+Insert into lawyer_specialization values (‘00004’, ‘family law’);
