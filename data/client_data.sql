1f1b110a2711135cab267d49f3477a8d22aaaeb8
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-08 16:56:40 -0500

    quote fix

--- data/client_data.sql
+++ data/client_data.sql
@@ -100,25 +100,25 @@ VALUES (
 insert into hearing (case_ID, judge, date_time, courthouse)
 values
 -- case_ID = 00001
-    (‘00001’, ’Fontenot’, ’2026-1-3 11:00:00.00’, ’Baton Rouge City Court’),
-    (‘00001’, ’Fontenot’, ’2026-5-30 09:00:00.00’, ’Baton Rouge City Court’),
+    ('00001', 'Fontenot', '2026-1-3 11:00:00.00', 'Baton Rouge City Court'),
+    ('00001', 'Fontenot', '2026-5-30 09:00:00.00', 'Baton Rouge City Court'),
 -- case_ID = 00002
-    (‘00002’, ’Johnson’, ’2026-1-11 14:30:00.00’, ’New Orleans City Court’),
-    (‘00002’, ’Johnson’, ’2026-4-20 09:30:00.00’, ’New Orleans City Court’),
+    ('00002', 'Johnson', '2026-1-11 14:30:00.00', 'New Orleans City Court'),
+    ('00002', 'Johnson', '2026-4-20 09:30:00.00', 'New Orleans City Court'),
 -- case_ID = 00003
-    (‘00003’, ’Smith’, ’2026-6-12 12:30:00.00’, ’Baton Rouge City Court’),
+    ('00003', 'Smith', '2026-6-12 12:30:00.00', 'Baton Rouge City Court'),
 -- case_ID = 00005
-    (‘00005’, ’Broussard’, ’2026-5-2 11:00:00.00’, ’Lafayette City Court’),
-    (‘00005’, ’Broussard’, ’2026-6-27 09:30:00.00’, ’Lafayette City Court’),
+    ('00005', 'Broussard', '2026-5-2 11:00:00.00', 'Lafayette City Court'),
+    ('00005', 'Broussard', '2026-6-27 09:30:00.00', 'Lafayette City Court'),
 -- case_ID = 00006
-    (‘00006’, ’Williams’, ’2026-6-25 11:00:00.00’, ’New Iberia City Court’),
+    ('00006', 'Williams', '2026-6-25 11:00:00.00', 'New Iberia City Court'),
 -- case_ID = 00008
-    (‘00008’, ’Boudreaux’, ’2026-1-29 10:30:00.00’, ’Lafayette City Court’),
-    (‘00008’, ’Hebert’, ’2026-4-6 12:30:00.00’, ’Lafayette City Court’),
-    (‘00008’, ’Hebert’, ’2026-5-2 15:30:00.00’, ’Lafayette City Court’),
+    ('00008', 'Boudreaux', '2026-1-29 10:30:00.00', 'Lafayette City Court'),
+    ('00008', 'Hebert', '2026-4-6 12:30:00.00', 'Lafayette City Court'),
+    ('00008', 'Hebert', '2026-5-2 15:30:00.00', 'Lafayette City Court'),
 -- case_ID = 00014
-    (‘00014’, ’Bourgeois’, ’2026-2-08 13:00:00.00’, ’New Iberia City Court’),
+    ('00014', 'Bourgeois', '2026-2-08 13:00:00.00', 'New Iberia City Court'),
 -- case_ID = 00015
-    (‘00015’, ’LeBlanc’, 2026-3-3 09:00:00.00, ’New Orleans City Court’),
-    (‘00015’, ’LeBlanc’, ’2026-3-27 15:00:00.00’, ’New Orleans City Court’),
-    (‘00015’, ’Guidry’, ’2026-5-5 11:30:00.00’, ’New Orleans City Court’);
+    ('00015', 'LeBlanc', '2026-3-3 09:00:00.00', 'New Orleans City Court'),
+    ('00015', 'LeBlanc', '2026-3-27 15:00:00.00', 'New Orleans City Court'),
+    ('00015', 'Guidry', '2026-5-5 11:30:00.00', 'New Orleans City Court');
