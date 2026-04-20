1a7d523004e77e0c120c767be54cf87af0cb6c4d
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-20 15:21:16 -0500

    Update lawyer_assignment.sql
    added on delete cascade for lawyer id

--- schema/lawyer_assignment.sql
+++ schema/lawyer_assignment.sql
@@ -4,6 +4,6 @@ lawyer_ID                   INT,
 case_ID                      INT,
 
 Primary key(lawyer_ID, case_ID),
-Foreign key (lawyer_ID) references lawyer(lawyer_ID),
+Foreign key (lawyer_id) references lawyer(lawyer_id) on delete cascade,
 Foreign key(case_ID) references `case`(case_id)
 );
