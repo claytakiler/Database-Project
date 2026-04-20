01ea4b45cf929250e529226ac834cc50f82a24f8
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-20 16:10:04 -0500

    restored backticks

--- schema/lawyer_assignment.sql
+++ schema/lawyer_assignment.sql
@@ -5,5 +5,5 @@ case_ID                      INT,
 
 Primary key(lawyer_ID, case_ID),
 Foreign key (lawyer_id) references lawyer(lawyer_id) on delete cascade,
-Foreign key (case_ID) references case(case_id) on delete cascade
+Foreign key (case_ID) references `case`(case_id) on delete cascade
 );
