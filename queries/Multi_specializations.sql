536dc50dd7ac08683fbea642890cde2f05b4582d
MNguyen2003 <matty52nguyen@gmail.com>
2026-04-21 12:09:46 -0500

    fixed the queries error for specializations

--- queries/Multi_specializations.sql
+++ queries/Multi_specializations.sql
@@ -1,5 +1,4 @@
---Query 1: Get the name and number of specializations for every lawyer who has more than one specialization, ordered by the number of specializations in descending order.
-select lawyer.name, lawyer.lawyer_ID, count(lawyer_specialization.specialization) as num_specializations
+select lawyer.name, lawyer.lawyer_ID, count(ls.specialization) as num_specializations
 from lawyer
 join lawyer_specialization ls on lawyer.lawyer_ID = ls.lawyer_ID
 group by lawyer.lawyer_ID, lawyer.name
