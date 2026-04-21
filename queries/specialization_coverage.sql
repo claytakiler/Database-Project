e1d31ce13a600821a653ffd6cb8ea7827f74f151
MNguyen2003 <matty52nguyen@gmail.com>
2026-04-21 11:04:19 -0500

    yesyes

--- queries/specialization_coverage.sql
+++ queries/specialization_coverage.sql
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 --Query 3: For each specialization, show how many lawyers cover it
-=======
->>>>>>> df5d85d (yes)
 select ls.specialization, count(ls.lawyer_ID) as num_lawyers
 from lawyer_specialization ls
 group by ls.specialization
