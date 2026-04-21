058155ec0b757987364b5ffa1e2fd8228116420f
Lwachterr <lwacht3@lsu.edu>
2026-04-21 11:05:10 -0500

    added 2 queries

--- schema/lawyer_assignment.sql
+++ schema/lawyer_assignment.sql
@@ -1,12 +1,7 @@
 
 Create table lawyer_assignment(
-<<<<<<< HEAD
-lawyer_ID                   INT,
-case_ID                      INT,
-=======
 lawyer_ID                  INT,
 case_ID                      INT,
->>>>>>> 7991d37 (added commas)
 
 Primary key(lawyer_ID, case_ID),
 Foreign key (lawyer_id) references lawyer(lawyer_id) on delete cascade,
