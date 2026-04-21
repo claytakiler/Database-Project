994b9475db4512c0935113eb4ed45dc07cd3ef47
MNguyen2003 <matty52nguyen@gmail.com>
2026-04-21 11:04:19 -0500

    yesyes

--- queries/lawyers_by_specialization.sql
+++ queries/lawyers_by_specialization.sql
@@ -1,3 +1,4 @@
+--Query 2: List the names, titles, salaries, and specializations of all lawyers, ordered by name and specialization.
 select lawyer.name, lawyer.title, lawyer.salary, ls.specialization
 from lawyer
 join lawyer_specialization ls on lawyer.lawyer_ID = ls.lawyer_ID
