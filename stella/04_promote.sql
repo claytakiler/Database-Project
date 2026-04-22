c239b534b4db5ad4b95f5570a13714b0099563e7
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-22 13:24:51 -0500

    promote improvement

--- stella/04_promote.sql
+++ stella/04_promote.sql
@@ -1,11 +1,13 @@
--- promotes associates working on 3 or more cases to partner
+-- promotes associates with 3 or more closed cases to partner
 
 update lawyer
 set title = 'partner'
 where title = 'associate'
   and lawyer_id in (
-      select lawyer_id
+      select lawyer_assignment.lawyer_id
       from lawyer_assignment
-      group by lawyer_id
-      having count(*) >= 3
+      join `case`on lawyer_assignment.case_id = `case`.case_id
+      where `case`.status = 'closed'
+      group by lawyer_assignment.lawyer_id
+      having COUNT(*) >= 3
   );
