95e0173905ae7fc76f4d6e69e146c8fe3cbe9026
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-22 13:41:50 -0500

    better comments

--- stella/04_promote.sql
+++ stella/04_promote.sql
@@ -2,12 +2,17 @@
 
 update lawyer
 set title = 'partner'
-where title = 'associate'
-  and lawyer_id in (
+where
+    title = 'associate'
+    -- get IDs of lawyers with 3 or more closed case assignments
+    and lawyer_id in (
       select lawyer_assignment.lawyer_id
-      from lawyer_assignment
-      join `case`on lawyer_assignment.case_id = `case`.case_id
+      -- join assignments with cases to access case status
+      from lawyer_assignment join `case` on lawyer_assignment.case_id = `case`.case_id
+      -- keep only assignments to closed cases
       where `case`.status = 'closed'
+      -- group assignments by lawyer
       group by lawyer_assignment.lawyer_id
-      having COUNT(*) >= 3
+      -- keep only lawyers with at least 3 closed case assignments
+      having count(*) >= 3
   );
