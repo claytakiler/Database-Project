56c8e1f8f115f27f5ecc287e95a5fc7f5f081012
clayton <claytonhouser10@gmail.com>
2026-04-23 14:33:13 -0500

    Formating code

--- queries/every_specialization.sql
+++ queries/every_specialization.sql
@@ -29,10 +29,15 @@ WITH
                     )
             )
     )
-SELECT CasesWithAllSpec.case_ID, lawyer.name AS lawyer_name, lawyer_specialization.specialization AS lawyer_specialization, `case`.title AS case_title FROM CasesWithAllSpec
-INNER JOIN `case` ON CasesWithAllSpec.case_ID = `case`.case_id
-INNER JOIN lawyer_assignment ON lawyer_assignment.case_ID = CasesWithAllSpec.case_ID
-INNER JOIN lawyer ON lawyer.lawyer_ID = lawyer_assignment.lawyer_ID
-INNER JOIN lawyer_specialization ON lawyer_specialization.lawyer_ID = lawyer.lawyer_ID
-ORDER BY CasesWithAllSpec.case_ID ASC, lawyer_specialization.specialization ASC
-;
+SELECT
+    CasesWithAllSpec.case_ID,
+    lawyer.name AS lawyer_name,
+    lawyer_specialization.specialization AS lawyer_specialization,
+    `case`.title AS case_title
+FROM
+    CasesWithAllSpec
+    INNER JOIN `case` ON CasesWithAllSpec.case_ID = `case`.case_id
+    INNER JOIN lawyer_assignment ON lawyer_assignment.case_ID = CasesWithAllSpec.case_ID
+    INNER JOIN lawyer ON lawyer.lawyer_ID = lawyer_assignment.lawyer_ID
+    INNER JOIN lawyer_specialization ON lawyer_specialization.lawyer_ID = lawyer.lawyer_ID
+ORDER BY CasesWithAllSpec.case_ID ASC, lawyer_specialization.specialization ASC;
