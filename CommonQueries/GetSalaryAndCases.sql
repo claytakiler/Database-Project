e32fb01458d5af405dbe4fb1f5a43645400575f2
clayton <claytonhouser10@gmail.com>
2026-04-23 17:03:11 -0500

    Adding finished salaryUpdate

---
+++ CommonQueries/GetSalaryAndCases.sql
@@ -0,0 +1,6 @@
+--Get the name, salary, title, and number of cases closed or settled
+SELECT name, salary, lawyer.title, COUNT(lawyer_assignment.case_ID) AS closed_or_settled_cases FROM lawyer
+INNER JOIN lawyer_assignment ON lawyer_assignment.lawyer_ID = lawyer.lawyer_ID
+INNER JOIN `case` ON lawyer_assignment.case_ID = `case`.case_id
+WHERE `case`.status = 'settled' or `case`.status = 'closed'
+GROUP BY lawyer.lawyer_ID;
