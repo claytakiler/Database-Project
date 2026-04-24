7e268627dc9c11b5c323a190ac7a0c12c0082ad0
MNguyen2003 <matty52nguyen@gmail.com>
2026-04-24 17:21:15 -0500

    made the specialization queries more complex

--- queries/Multi_specializations.sql
+++ queries/Multi_specializations.sql
@@ -1,6 +1,13 @@
-select lawyer.name, lawyer.lawyer_ID, count(ls.specialization) as num_specializations
+select lawyer.name,
+       lawyer.lawyer_ID,
+       lawyer.title,
+       format(lawyer.salary, 2) as salary,
+       count(distinct ls.specialization) as num_specializations,
+       group_concat(ls.specialization order by ls.specialization separator ' | ') as specializations_list,
+       count(distinct la.case_ID) as num_cases_assigned
 from lawyer
 join lawyer_specialization ls on lawyer.lawyer_ID = ls.lawyer_ID
-group by lawyer.lawyer_ID, lawyer.name
-having count(ls.specialization) > 1
-order by num_specializations desc;
+left join lawyer_assignment la on lawyer.lawyer_ID = la.lawyer_ID
+group by lawyer.lawyer_ID, lawyer.name, lawyer.title, lawyer.salary
+having count(distinct ls.specialization) > 1
+order by num_specializations desc, num_cases_assigned desc;
