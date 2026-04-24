699b84a51a55fe834899321b19347830fd53deb7
clayton <claytonhouser10@gmail.com>
2026-04-24 17:03:32 -0500

    Adding constraints to case for NOT NULL

--- schema/case.sql
+++ schema/case.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `case`(
     case_id INT AUTO_INCREMENT PRIMARY KEY,
-    client_id INT,
-    title VARCHAR(100),
+    client_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
     status ENUM('open', 'in trial', 'on appeal', 'settled', 'closed') DEFAULT 'open',
     FOREIGN KEY (client_id) REFERENCES client(client_id)
 );
