a0111630a06eb06fa16ff44287489e949c690d04
Lwachterr <lwacht3@lsu.edu>
2026-04-24 17:29:02 -0500

    updated queries

--- queries/cases_assigned_to_lawyers.sql
+++ queries/cases_assigned_to_lawyers.sql
@@ -1,6 +1,15 @@
-SELECT
-    lawyer_assignment.lawyer_ID,     
-    COUNT(case_ID) AS total_cases 
+SELECT 
+    lawyer_assignment.lawyer_ID,
+    lawyer.name AS lawyer_name,
+    lawyer.title,
+    COUNT(lawyer_assignment.case_ID) AS total_cases
+   
 FROM lawyer_assignment
-GROUP BY lawyer_ID   
-ORDER BY total_cases DESC;  
+INNER JOIN lawyer
+    ON lawyer_assignment.lawyer_ID = lawyer.lawyer_ID
+GROUP BY 
+    lawyer_assignment.lawyer_ID,
+    lawyer.name,
+    lawyer.title
+HAVING COUNT(lawyer_assignment.case_ID) > 1
+ORDER BY total_cases DESC;
