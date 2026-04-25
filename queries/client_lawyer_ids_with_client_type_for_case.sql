c79c2062e4afbb46bfc94def66245536d5962cd9
ashes2ashesdus2dus <awil678@lsu.com>
2026-04-24 21:01:38 -0500

    update to both my queries

--- queries/client_lawyer_ids_with_client_type_for_case.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    la.lawyer_ID,
-    c.case_ID,
-    cl.client_ID,
-    cl.type AS client_type
-FROM lawyer_assignment la
-JOIN `case` c ON la.case_ID = c.case_ID
-JOIN client cl ON c.client_ID = cl.client_ID;
