295b30fdc621f8342f867aee281df033d30e022c
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-04-30 07:56:30 -0500

    Update Multi_specializations.sql

--- queries/Multi_specializations.sql
+++ queries/Multi_specializations.sql
@@ -1,10 +1,9 @@
-select lawyer.name,
-       lawyer.lawyer_ID,
-       lawyer.title,
-       format(lawyer.salary, 2) as salary,
-       count(distinct ls.specialization) as num_specializations,
-       group_concat(ls.specialization order by ls.specialization separator ' | ') as specializations_list,
-       count(distinct la.case_ID) as num_cases_assigned
+select
+lawyer.name,
+lawyer.lawyer_ID,
+	count(distinct ls.specialization) as num_specializations,
+	count(distinct la.case_ID) as num_cases_assigned,
+	group_concat(distinct ls.specialization order by ls.specialization separator ' | ') as specializations_list
 from lawyer
 join lawyer_specialization ls on lawyer.lawyer_ID = ls.lawyer_ID
 left join lawyer_assignment la on lawyer.lawyer_ID = la.lawyer_ID
